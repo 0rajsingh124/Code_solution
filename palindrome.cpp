cc53e354907526aabcc1736c6beda8ad660d2fd1
Raj Singh <116282944+0rajsingh124@users.noreply.github.com>
2023-10-29 10:35:05 +0530

    palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,4 +1,4 @@
-/*#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -18,18 +18,4 @@ int main()
     cout<<"not palindrome";
 
 
-}*/
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    int a,b,c,sum=0;
-    cin>>a;
-    while(a!=0)
-    {
-    sum = (sum*10)+a%10;
-    a=a/10;
-    }
-    cout<<sum;
 }
